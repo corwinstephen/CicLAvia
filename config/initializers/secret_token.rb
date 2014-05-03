260543ab67065e1fe4164be4efab618ead71d6b4
Stephen Corwin <corwinstephen@gmail.com>
2014-05-03 14:31:24 -0700

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CicLAvia::Application.config.secret_key_base = '5d52448a2b6e83df923f3e56994ff29da364636421ad50fadf2a550e88f0b6f1b20ffbea59e6ca650dab6c740eba5dd9574e1b885ec9baa79775c612c6b4335d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CicLAvia::Application.config.session_store :cookie_store, key: '_CicLAvia_session'

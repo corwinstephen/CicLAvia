81472c57a557f0b0d32dc279f879bcf7aba45431
Stephen Corwin <corwinstephen@gmail.com>
2014-05-07 23:49:31 -0700

    Turbolinks js support

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,11 +3,20 @@
   _.mixin(_.str.exports());
 
   Ciclavia.init = function(){
-    var currentController = _.capitalize(Ciclavia.Globals.controllerName);
-    var currentAction = _.camelize(Ciclavia.Globals.actionName);
+    var initByControllerAction = function(){
+      var currentController = _.capitalize(Ciclavia.Globals.controllerName);
+      var currentAction = _.camelize(Ciclavia.Globals.actionName);
 
-    if(_.isObject(Ciclavia.Controllers[currentController]) && _.isFunction(Ciclavia.Controllers[currentController][currentAction])){
-      Ciclavia.Controllers[currentController][currentAction]();
-    }
+      if(_.isObject(Ciclavia.Controllers[currentController]) && _.isFunction(Ciclavia.Controllers[currentController][currentAction])){
+        Ciclavia.Controllers[currentController][currentAction]();
+      }
+    };
+
+    initByControllerAction();
+
+    // We need this for JS to work with Turbolinks
+    $(document).on("page:load", function(){
+      initByControllerAction();
+    });
   };
 })(jQuery, _);

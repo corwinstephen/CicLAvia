ad5ad82685109234f65c0e8e290920e103065712
Stephen Corwin <corwinstephen@gmail.com>
2014-05-07 19:34:23 -0700

    Added map model and some routes

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'haml'
 
 group :doc do
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ CicLAvia::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'maps#index'
+
+  resources :maps
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

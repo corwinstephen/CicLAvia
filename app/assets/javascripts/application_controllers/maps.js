56ea480e9bceea32a80ee5754896dd19fc106ec8
Stephen Corwin <corwinstephen@gmail.com>
2014-05-26 14:34:55 -0700

    Added logo. Got rid of map info

--- app/assets/javascripts/application_controllers/maps.js
+++ app/assets/javascripts/application_controllers/maps.js
@@ -8,8 +8,10 @@
         height: $(window).height()
       });
 
-      var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
-        .setView([34.048776, -118.251522], 11);
+      var map = L.mapbox.map('map', 'corwinstephen.i6aocpam', {
+        infoControl: false,
+      })
+      .setView([34.048776, -118.251522], 11);
 
       map.on('click', function(e) {
           console.log(e.containerPoint.toString() + ', ' + e.latlng.toString());

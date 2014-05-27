f48aa510db79e02536638b1ec5724772a668a761
Stephen Corwin <corwinstephen@gmail.com>
2014-05-27 08:05:05 -0700

    Use postgres

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.0.2'
-gem 'sqlite3'
+gem 'pg'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140508050204) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "maps", force: true do |t|
     t.string   "name"
     t.text     "description"
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 20140508050204) do
     t.datetime "updated_at"
   end
 
-  add_index "points", ["map_id"], name: "index_points_on_map_id"
+  add_index "points", ["map_id"], name: "index_points_on_map_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140508050204) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

0ed9b5382d58ac890f8b9b7f92a9b9e74cc2032b
Stephen Corwin <corwinstephen@gmail.com>
2014-07-20 23:42:26 -0700

    Refactored

--- lib/route_importer.rb
+++ lib/route_importer.rb
@@ -12,16 +12,9 @@ class RouteImporter
 
       @parser.each_placemark do |placemark|
         if placemark.type == :LineString
-          route.route_segments.build({
-            name: placemark.name,
-            coordinate_array: placemark.coordinates
-          })
+          build_route_segment_from_placemark(route, placemark)
         elsif placemark.type == :Point
-          route.places.build({
-            name: placemark.name,
-            lat: placemark.coordinates.first[0],
-            lng: placemark.coordinates.first[1]
-          })
+          build_place_from_placemark(route, placemark)
         else
           raise "Don't know how to import placemark type #{placemark.type}"
         end
@@ -29,7 +22,23 @@ class RouteImporter
 
       return route.save
     end
+  end
 
+  private
+
+  def build_route_segment_from_placemark(route, placemark)
+    return route.route_segments.build({
+      name: placemark.name,
+      coordinate_array: placemark.coordinates
+    })
+  end
+
+  def build_place_from_placemark(route, placemark)
+    return route.places.build({
+      name: placemark.name,
+      lat: placemark.coordinates.first[0],
+      lng: placemark.coordinates.first[1]
+    })
   end
 
 end

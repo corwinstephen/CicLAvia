691d3dd86fa1ad868f5eec239636469e196e0dd5
Stephen Corwin <corwinstephen@gmail.com>
2014-08-04 22:33:30 -0700

    Routes clickable

--- app/assets/javascripts/modules/dialogues/route-dialogue.js
+++ app/assets/javascripts/modules/dialogues/route-dialogue.js
@@ -2,17 +2,34 @@
   Ciclavia.Modules.RouteDialogue = Stapes.subclass({
     template: 'route-dialogue',
 
+    dialogue: null,
     $dialogue: null,
 
     constructor: function(route){
-      this.$dialogue = HandlebarsTemplates[this.template]({
+      this.dialogue = HandlebarsTemplates[this.template]({
         name: route.name
       });
+      this.$dialogue = $(this.dialogue);
+
+      this._setEvents();
     },
 
     show: function(){
       Ciclavia.Modules.Blackout.on();
       $("body").append(this.$dialogue);
+    },
+
+    hide: function(){
+      Ciclavia.Modules.Blackout.off();
+      this.$dialogue.remove();
+    },
+
+    _setEvents: function(){
+      this.$dialogue.find(".dialogue-close").click(this._closeButtonClicked.bind(this));
+    },
+
+    _closeButtonClicked: function(){
+      this.hide();
     }
   });
 })(Stapes);

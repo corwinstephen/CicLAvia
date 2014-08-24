736af6902fe7ee8aeae44a8199af48d69b9baf93
Stephen Corwin <corwinstephen@gmail.com>
2014-08-24 16:19:54 -0700

    Refactor route create

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,17 +2,8 @@ class RoutesController < ApplicationController
   respond_to :json
 
   def create
-    route = Route.new(route_attributes)
-    
-    # Temporary, until the event system gets built out
-    route.event = Event.first
-    route_segments_data = JSON.parse(params[:route][:route_segments])
-    route_segments_data.each do |segment_data|
-      route.route_segments.build(coordinate_array: segment_data["coordinate_array"])
-    end
-
+    route = build_route
     route.save
-
     respond_with route
   end
 
@@ -26,6 +17,24 @@ class RoutesController < ApplicationController
   end
 
   private
+
+  def build_route
+    route = Route.new(route_attributes)
+    
+    # Temporary, until the event system gets built out
+    route.event = Event.first
+    route_segments_data.each do |segment_data|
+      route.route_segments.build(coordinate_array: segment_data["coordinate_array"])
+    end
+
+    return route
+  end
+
+  def route_segments_data
+    JSON.parse(params[:route][:route_segments])
+  rescue
+    nil
+  end
 
   def route_attributes
     params.require(:route).permit(:name, :description)

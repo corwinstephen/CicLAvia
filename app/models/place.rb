1e1b284c6226ac11ac184c14dc40ad72402210da
Stephen Corwin <corwinstephen@gmail.com>
2015-07-27 13:15:50 -0700

    No default image

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,8 +10,8 @@ class Place < ActiveRecord::Base
                     :styles => { :large => '500x300#', :medium => '250x150#', :thumb => '50x50#' },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
-                    :path => '/plces/:id/:style/:filename',
-                    :default_url => '/images/defaults/noavatar.svg',
+                    :path => '/places/:id/:style/:filename',
+                    :default_url => '',
                     :url => ':s3_domain_url'
 
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/                    
